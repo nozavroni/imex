31131c14712c9ce4a9aa062c5d62e41f8dc8d957
Luke Visinoni <luke.visinoni@gmail.com>
2016-12-19 03:25:10 -0800

    Still attempting to get test coverage services working - refs #10

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo 'date.timezone = "America/Los_Angeles"' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
+
+mv phpunit.xml.dist phpunit.xml
--- travis/script.sh
+++ travis/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PHP_VERSION != "7.0" ]]; then
-  vendor/bin/phpunit --coverage-text --coverage-clover ./build/logs/clover.xml
+  vendor/bin/phpunit -c phpunit.xml --coverage-text --coverage-clover ./build/logs/clover.xml
 else
-  vendor/bin/phpunit
+  vendor/bin/phpunit -c phpunit.xml
 fi
