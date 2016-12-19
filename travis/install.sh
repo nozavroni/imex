a559c7fabe6e9da6bed869bc53e975b4823d88d5
Luke Visinoni <luke.visinoni@gmail.com>
2016-12-19 03:17:12 -0800

    Attempting to get coveralls working - refs #10

--- travis/install.sh
+++ travis/install.sh
@@ -2,6 +2,6 @@
 composer install -n
 
 # The php-coveralls package does not work for PHP7
-if [[ $TRAVIS_PHP_VERSION == "5.6" ]]; then
+if [[ $TRAVIS_PHP_VERSION != "7.0" ]]; then
   composer require --dev satooshi/php-coveralls:~0.7@dev
 fi
