d591b7508411a9c1374834566f425eeaf07db3cc
Luke Visinoni <luke.visinoni@gmail.com>
2016-12-19 02:46:18 -0800

    Added namespace to main bundle test * references #10

--- src/Nozavroni/IMEX/ImportBundle/Tests/NozavroniIMEXImportBundleTest.php
+++ src/Nozavroni/IMEX/ImportBundle/Tests/NozavroniIMEXImportBundleTest.php
@@ -7,7 +7,10 @@
  * @copyright Copyright (c) 2016 Luke Visinoni <luke.visinoni@gmail.com>
  * @author    Luke Visinoni <luke.visinoni@gmail.com>
  */
+namespace Nozavroni\IMEX\ImportBundle\Test;
+
 use Nozavroni\IMEX\ImportBundle\NozavroniIMEXImportBundle;
+use PHPUnit_Framework_TestCase;
 
 class NozavroniIMEXImportBundleTest extends PHPUnit_Framework_TestCase
 {
