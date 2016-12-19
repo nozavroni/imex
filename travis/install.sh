4c6322b12ef160699b460b8a0045867f489ddf7c
Luke Visinoni <luke.visinoni@gmail.com>
2016-12-18 22:57:24 -0800

    Fixed travis configuration

--- travis/install.sh
+++ travis/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 composer install -n
 
-#if [[ $TRAVIS_PHP_VERSION == "5.6" ]]; then
+# The php-coveralls package does not work for PHP7
+if [[ $TRAVIS_PHP_VERSION == "5.6" ]]; then
   composer require --dev satooshi/php-coveralls:~0.7@dev
-#fi
+fi
