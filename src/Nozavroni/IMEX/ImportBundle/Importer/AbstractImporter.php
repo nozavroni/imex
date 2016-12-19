2c76d25163b6f306d31786a16bbc0f36da4406a8
Luke Visinoni <luke.visinoni@gmail.com>
2016-12-19 10:49:27 +0000

    Apply fixes from StyleCI

--- src/Nozavroni/IMEX/ImportBundle/Importer/AbstractImporter.php
+++ src/Nozavroni/IMEX/ImportBundle/Importer/AbstractImporter.php
@@ -4,12 +4,13 @@
  * A set of components for writing import/export scripts.
  *
  * @version   {$VERSION}
+ *
  * @copyright Copyright (c) 2016 Luke Visinoni <luke.visinoni@gmail.com>
  * @author    Luke Visinoni <luke.visinoni@gmail.com>
  */
+
 namespace Nozavroni\IMEX\ImportBundle\Importer;
 
 abstract class AbstractImporter
 {
-
 }
