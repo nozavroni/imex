57f186c1f8c75e1efc4738b5d06e4afda003d4b7
Luke Visinoni <luke.visinoni@gmail.com>
2016-12-19 03:16:30 -0800

    Attempting to get coveralls working - refs #10

--- travis/after_script.sh
+++ travis/after_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The php-coveralls package does not work for PHP7
-if [[ $TRAVIS_PHP_VERSION == "5.6" ]]; then
+if [[ $TRAVIS_PHP_VERSION != "7.0" ]]; then
   vendor/bin/coveralls -v
   wget https://scrutinizer-ci.com/ocular.phar
   php ocular.phar code-coverage:upload --format=php-clover ./build/logs/clover.xml
